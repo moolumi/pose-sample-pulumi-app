25c18bdd03f4bd1f5db439f17bcf5d6d2bf397c7
Neo <neo@pulumi.com>
2025-10-22 11:37:38 +0000

    preview changes - neo-preview-1761133058589

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-// TODO This link is broken.
 // Import the [pulumi/aws](https://pulumi.io/reference/pkg/nodejs/@pulumi/aws/index.html) package
-// TODO: Ah I was using TS instead of JS.
-// TODO: Oops I got the wrong one. I need to use @pulumi/aws-apigateway.
-// I'm really confused: what's the difference between aws and aws-apigateway?
 const pulumi = require("@pulumi/pulumi");
 // Subset of `awsx` (v1). Another component provider. Wraps `aws`.
 // High-level construct.
 // L2
-// SUGGESTION: awsx.apigateway? So it reduces the confusion
 const apigateway = require("@pulumi/aws-apigateway");
 // Primitives. Expose primitives. REST-ful provider version. Generated.
 // API in provider form. Creates resources in AWS.
 // L1
-// Version:
 const aws = require("@pulumi/aws");
 // Crosswalk / higher level abstactions/wrapper around some parts of AWS
 // best practices, etc. Hand-written.
 // L2
-// Version:
 const awsx = require("@pulumi/awsx");
 const { Runtime } = require("@pulumi/aws/lambda");
-awsx.apigateway
-// TODO: Where is this?
-// const apigateway = require("@pulumi/aws-apigateway");
 
+
+// Create S3 bucket for access logs
+const logsBucket = new aws.s3.Bucket("api-logs", {
+    // Enable versioning for the logs bucket
+    versioning: {
+        enabled: true,
+    },
+});
+
+// Create S3 bucket for static files with logging enabled
+const staticFilesBucket = new aws.s3.Bucket("api-static-files", {
+    // Enable versioning
+    versioning: {
+        enabled: true,
+    },
+    // Enable server-side encryption
+    serverSideEncryptionConfiguration: {
+        rule: {
+            applyServerSideEncryptionByDefault: {
+                sseAlgorithm: "AES256",
+            },
+        },
+    },
+    // Enable access logging
+    logging: {
+        targetBucket: logsBucket.bucket,
+        targetPrefix: "access-logs/",
+    },
+});
+
+// Upload static files to the bucket
+const indexFile = new aws.s3.BucketObject("index.html", {
+    bucket: staticFilesBucket.id,
+    source: new pulumi.asset.FileAsset("www/index.html"),
+    contentType: "text/html",
+});
+
+const faviconFile = new aws.s3.BucketObject("favicon.png", {
+    bucket: staticFilesBucket.id,
+    source: new pulumi.asset.FileAsset("www/favicon.png"),
+    contentType: "image/png",
+});
 
 // A Lambda function to invoke.
 const eventHandler = new aws.lambda.CallbackFunction("handler", {
@@ -38,30 +70,27 @@ const eventHandler = new aws.lambda.CallbackFunction("handler", {
     },
 });
 
-// Create a public HTTP endpoint (using AWS APIGateway)
-// TODO This was awsx on the example, but it's not working.
-// const endpoint = new aws.apigateway.API("hello", {
-//     routes: [
-//         // Serve static files from the `www` folder (using AWS S3)
-//         {
-//             path: "/",
-//             localPath: "www",
-//         },
+// Make the static files bucket publicly readable
+const bucketPolicy = new aws.s3.BucketPolicy("api-static-files-policy", {
+    bucket: staticFilesBucket.id,
+    policy: staticFilesBucket.id.apply(bucketName => JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+            {
+                Effect: "Allow",
+                Principal: "*",
+                Action: "s3:GetObject",
+                Resource: `arn:aws:s3:::${bucketName}/*`,
+            },
+        ],
+    })),
+});
 
-//         // Serve a simple REST API on `GET /name` (using AWS Lambda)
-//         {
-//             path: "/source",
-//             method: "GET",
-//             eventHandler,
-//         },
-//     ],
-// });
-// Ahhh I was using the wrong one. It's not. It's RESTAPI.
+// Configure bucket for static website hosting (using bucket website configuration)
+// Note: Using bucket policy to make it publicly accessible instead of BucketWebsite
+// which may not be available in this AWS provider version
 
 // A REST API to route requests to the Lambda function.
-// TODO Wrong: it's RestApi, not RestAPI.
-// https://www.pulumi.com/docs/iac/clouds/aws/guides/api-gateway/
-// const endpoint = new aws.apigateway.RestApi("api", {
 const endpoint = new apigateway.RestAPI("api", {
     routes: [
         {
@@ -69,13 +98,12 @@ const endpoint = new apigateway.RestAPI("api", {
             method: "GET",
             eventHandler,
         },
-        {
-            path: "/",
-            localPath: "www",
-        },
+        // Remove the static file serving from RestAPI since we'll use S3 directly
+        // The S3 bucket with logging is now available at the website endpoint
     ],
 });
 
 
-// Export the public URL for the HTTP service
-exports.url = endpoint.url;
+// Export the public URLs
+exports.apiUrl = endpoint.url;
+exports.staticBucketName = staticFilesBucket.id;
